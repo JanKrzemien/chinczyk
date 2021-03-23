d93bba070c27e189bcba693dfab7fe07ee464c84
JanKrzemien <68891867+JanKrzemien@users.noreply.github.com>
2021-03-23 11:13:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var http = require("http")
-var fs = require("filesystem")
+var fs = require("fs")
 
 var server = http.createServer(function (req, res) {
     switch(req.method){
         case "GET":
-            fs.readFile("/index.html", function(error, data){
+            fs.readFile("index.html", function(error, data){
                 res.writeHead(200, {"Content-Type":"text/html;charset=utf-8"})
                 res.send(data)
                 res.end()
