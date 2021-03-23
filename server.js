d0849b61dc1fff448b0851caba2a9e5d62e4e600
JanKrzemien <68891867+JanKrzemien@users.noreply.github.com>
2021-03-23 11:17:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var server = http.createServer(function (req, res) {
                 res.end()
             })                
             break;
-        case: "POST":
+        case "POST":
             
             break;
     }
