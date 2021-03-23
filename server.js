aef120062764dda49bed143e64a484ed04108ac6
JanKrzemien <68891867+JanKrzemien@users.noreply.github.com>
2021-03-23 11:09:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var http = require("http")
+var fs = require("filesystem")
 
 var server = http.createServer(function (req, res) {
+    switch(req.method){
+        case "GET":
+            fs.readFile("/index.html", function(error, data){
+                res.writeHead(200, {"Content-Type":"text/html;charset=utf-8"})
+                res.send(data)
+                res.end()
+            })                
+            break;
+        case: "POST":
+            
+            break;
+    }
     console.log(JSON.stringify(req.headers, null, 5));
 })
 server.listen(3000, function () { console.log("OK") })
